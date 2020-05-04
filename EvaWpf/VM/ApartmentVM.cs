9b8731b6df39fe032406607df7108bab4b830e43
adelvig-an <63202963+adelvig-an@users.noreply.github.com>
2020-05-04 21:14:01 +0700

    1

--- EvaWpf/VM/ApartmentVM.cs
+++ EvaWpf/VM/ApartmentVM.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace EvaWpf.VM
 {
     class ApartmentVM : PageVM
     {
+        public IEnumerable<string> TehDocuments { get; set; }
+
+        public ICommand AddCommand { get; }
     }
 }
